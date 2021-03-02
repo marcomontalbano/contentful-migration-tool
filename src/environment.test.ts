8debd3151167d414bddd61c3593dd74d7547bd38
Marco Montalbano <marcomontalbano.work@gmail.com>
2021-03-02 22:16:46 +0100

    Add more test for getNewEnvironmentName

--- src/environment.test.ts
+++ src/environment.test.ts
@@ -5,13 +5,9 @@ import * as contentful from './contentful'
 
 describe('Environment', () => {
 
-    beforeEach(() => {
-        process.env = Object.assign(process.env, {
-            CONTENT_MANAGEMENT_TOKEN: 'management token',
-            SPACE_ID: 'space id',
-            ENVIRONMENT: 'master'
-        });
+    const envSaved = { ...process.env }
 
+    beforeEach(() => {
         jest.spyOn(contentful, 'getSpace').mockResolvedValue({
             ...{} as Space,
             getEnvironmentAliases: async () => ({
@@ -27,8 +23,26 @@ describe('Environment', () => {
         })
     })
 
-    it('getNewEnvironmentName', async () => {
-        Date.now = jest.fn(() => 1614451488353);
-        expect(await environment.getNewEnvironmentName()).toEqual('master-2021.02-27-18.44.48')
+    afterEach(() => {
+        process.env = { ...envSaved }
+    })
+
+    describe('getNewEnvironmentName', () => {
+        it('should throw an error if ENVIRONMENT is not defined', () => {
+            expect.assertions(1);
+            return environment.getNewEnvironmentName().catch(e => expect(e).toEqual(new Error('The environment variable "ENVIRONMENT" is missing')))
+        })
+
+        it('should append date and time if the environment is an alias', async () => {
+            process.env.ENVIRONMENT = 'master'
+            Date.now = jest.fn(() => 1614451488353);
+            expect(await environment.getNewEnvironmentName()).toEqual('master-2021.02-27-18.44.48')
+        })
+
+        it('should not append date and time if the environment is not an alias', async () => {
+            process.env.ENVIRONMENT = 'test-env'
+            Date.now = jest.fn(() => 1614451488353);
+            expect(await environment.getNewEnvironmentName()).toEqual('test-env')
+        })
     })
 })
